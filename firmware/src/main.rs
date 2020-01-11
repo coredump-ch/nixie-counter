5765c64c921b410d6c3e493a9bfeb988077e0719
Danilo Bargen <mail@dbrgn.ch>
2020-01-11 14:27:40 +0100

    firmware: Listen to toggle pin interrupts

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -13,11 +13,18 @@ use embedded_hal::digital::v2::OutputPin;
 use rtfm::app;
 use stm32f1xx_hal::{prelude::*, pac};
 use stm32f1xx_hal::delay::Delay;
-use stm32f1xx_hal::gpio::{Output, PushPull, gpiob};
+use stm32f1xx_hal::gpio::{Edge, ExtiPin, Input, Output, PullUp, PushPull, gpioa, gpiob};
 
 #[app(device = stm32f1::stm32f103, peripherals = true)]
 const APP: () = {
     struct Resources {
+        // EXTI peripheral
+        EXTI: pac::EXTI,
+
+        // Buttons
+        btn_up: gpioa::PA11<Input<PullUp>>,
+        btn_dn: gpioa::PA8<Input<PullUp>>,
+
         // LEDs
         led_pwr: gpiob::PB3<Output<PushPull>>,
         led_wifi: gpiob::PB4<Output<PushPull>>,
@@ -48,6 +55,21 @@ const APP: () = {
 
         // Disable JTAG to free up pins PA15, PB3 and PB4 for normal use 
         let (_pa15, pb3, pb4) = afio.mapr.disable_jtag(gpioa.pa15, gpiob.pb3, gpiob.pb4);
+
+        // Set up toggle inputs
+        let mut btn_up = gpioa.pa11.into_pull_up_input(&mut gpioa.crh);
+        let mut btn_dn = gpioa.pa8.into_pull_up_input(&mut gpioa.crh);
+
+        // Enable toggle interrupts
+        macro_rules! enable_interrupt {
+            ($pin:expr) => {
+                $pin.make_interrupt_source(&mut afio);
+                $pin.enable_interrupt(&device.EXTI);
+                $pin.trigger_on_edge(&device.EXTI, Edge::FALLING);
+            }
+        }
+        enable_interrupt!(btn_up);
+        enable_interrupt!(btn_dn);
 
         // Clock configuration
         let clocks = rcc
@@ -91,9 +113,42 @@ const APP: () = {
 
         // Assign resources
         init::LateResources {
+            EXTI: device.EXTI,
+            btn_up,
+            btn_dn,
             led_pwr,
             led_wifi,
         }
+    }
+
+    #[idle]
+    fn idle(_: idle::Context) -> ! {
+        hprintln!("idle").unwrap();
+
+        // The idle loop
+        loop {}
+    }
+
+    #[task(binds = EXTI15_10, resources = [btn_up, led_wifi, EXTI])]
+    fn count_up(ctx: count_up::Context) {
+        hprintln!("Count up").unwrap();
+
+        // Clear the interrupt
+        ctx.resources.btn_up.clear_interrupt_pending_bit();
+
+        // Toggle the wifi led
+        ctx.resources.led_wifi.toggle().unwrap();
+    }
+
+    #[task(binds = EXTI9_5, resources = [btn_dn, led_pwr, EXTI])]
+    fn count_down(ctx: count_down::Context) {
+        hprintln!("Count down").unwrap();
+
+        // Clear the interrupt
+        ctx.resources.btn_dn.clear_interrupt_pending_bit();
+
+        // Toggle the pwr led
+        ctx.resources.led_pwr.toggle().unwrap();
     }
 
     // RTFM requires that free interrupts are declared in an extern block when
