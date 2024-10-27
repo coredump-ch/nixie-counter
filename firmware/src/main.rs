119182f84a542e14dd8707f1b887ab8be60389f1
Danilo Bargen <mail@dbrgn.ch>
2024-10-27 23:52:39 +0100

    Connect WiFi

--- firmware/src/main.rs
+++ firmware/src/main.rs
@@ -1,24 +1,49 @@
-use esp_idf_svc::hal::{gpio::{PinDriver, Pull}, prelude::Peripherals, task::block_on};
+use embedded_svc::wifi::{AuthMethod, ClientConfiguration, Configuration};
+use esp_idf_svc::{
+    eventloop::EspSystemEventLoop,
+    hal::{
+        gpio::{PinDriver, Pull},
+        prelude::Peripherals,
+        task::block_on,
+    },
+    nvs::EspDefaultNvsPartition,
+    timer::EspTaskTimerService,
+    wifi::{AsyncWifi, EspWifi},
+};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+const WIFI_SSID: &str = env!("WIFI_SSID");
+const WIFI_PASS: &str = env!("WIFI_PASS");
 
 fn main() -> anyhow::Result<()> {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_svc::sys::link_patches();
-
     // Bind the log crate to the ESP Logging facilities
     esp_idf_svc::log::EspLogger::initialize_default();
 
-    let peripherals = Peripherals::take()?;
-
-    let mut button = PinDriver::input(peripherals.pins.gpio9)?;
-    let mut led = PinDriver::output(peripherals.pins.gpio3)?;
-
-    button.set_pull(Pull::Up)?;
-
     log::info!("Starting nixie firmware v{VERSION}...");
 
+    let peripherals = Peripherals::take()?;
+    let sys_loop = EspSystemEventLoop::take()?;
+    let timer_service = EspTaskTimerService::new()?;
+    let nvs = EspDefaultNvsPartition::take()?;
+
+    // Set up I/O pins
+    let mut button = PinDriver::input(peripherals.pins.gpio9)?;
+    button.set_pull(Pull::Up)?;
+    let mut led = PinDriver::output(peripherals.pins.gpio3)?;
+
+    // Connect WiFi
+    let mut wifi = AsyncWifi::wrap(
+        EspWifi::new(peripherals.modem, sys_loop.clone(), Some(nvs))?,
+        sys_loop,
+        timer_service,
+    )?;
+    block_on(connect_wifi(&mut wifi))?;
+
+    // Main loop
     block_on(async {
         loop {
             button.wait_for_low().await?;
@@ -31,4 +56,31 @@ fn main() -> anyhow::Result<()> {
             log::info!("Released");
         }
     })
+}
+
+/// Connect to WiFi
+///
+/// Credentials must be passed in through env variables (WIFI_SSID and WIFI_PASS).
+async fn connect_wifi(wifi: &mut AsyncWifi<EspWifi<'static>>) -> anyhow::Result<()> {
+    let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
+        ssid: WIFI_SSID.try_into().unwrap(),
+        bssid: None,
+        auth_method: AuthMethod::WPA2Personal,
+        password: WIFI_PASS.try_into().unwrap(),
+        channel: None,
+        ..Default::default()
+    });
+
+    wifi.set_configuration(&wifi_configuration)?;
+
+    wifi.start().await?;
+    log::info!("Wifi started");
+
+    wifi.connect().await?;
+    log::info!("Wifi connected");
+
+    wifi.wait_netif_up().await?;
+    log::info!("Wifi netif up");
+
+    Ok(())
 }
